cf86e2af0e6eb0575746de196bce810b18c2b144
ksbhanegaonkar <kedarsbhanegaonkar@gmail.com>
2020-06-30 13:22:16 +0530

    Added foundation code for 3js and displayed cube

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -4,6 +4,7 @@ import Desktop from './Components/Desktop/Desktop';
 import LoginForm from './Components/LoginForm/LoginForm';
 import {BrowserRouter as Router,NavLink,Redirect,Route,Switch} from 'react-router-dom';
 import AuthenticatedComponent from './Components/AuthenticatedComponent/AuthenticatedComponent';
+import ShoppingMall from './Components/3JS/ShoppingMall';
 
 class App extends Component {
   state={
@@ -14,7 +15,7 @@ class App extends Component {
   return (
     <Router>
         <Switch>
-          <Route path="/" exact strict component={LoginForm}></Route>
+          <Route path="/" exact strict component={ShoppingMall}></Route>
           <AuthenticatedComponent>
           <Route path="/desktop" exact strict component={Desktop}></Route>
           </AuthenticatedComponent>
